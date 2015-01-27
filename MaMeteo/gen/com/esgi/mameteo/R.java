60a2b1e8f1a5d9b21cfc53cde9ea9ae27c0769b6
odasilva <odasilva@iut.univ-paris8.fr>
2015-01-27 19:45:24 +0100

    add JSon Parser & print Result for Paris

--- MaMeteo/gen/com/esgi/mameteo/R.java
+++ MaMeteo/gen/com/esgi/mameteo/R.java
@@ -13,6 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int cityText=0x7f060000;
+        public static final int condDescr=0x7f060002;
+        public static final int condIcon=0x7f060001;
+        public static final int hum=0x7f060007;
+        public static final int humLab=0x7f060006;
+        public static final int press=0x7f060005;
+        public static final int pressLab=0x7f060004;
+        public static final int temp=0x7f060003;
+        public static final int windDeg=0x7f06000a;
+        public static final int windLab=0x7f060008;
+        public static final int windSpeed=0x7f060009;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
